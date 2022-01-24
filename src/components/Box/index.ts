1ccde6c2bd7f0d22a9eec314d966a19cfec6dd0a
Antoni0o <antonio.dias8997@gmail.com>
2022-01-24 13:24:51 -0300

    fixed screen resizing

--- src/components/Box/index.ts
+++ src/components/Box/index.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
+  width: 80%;
   padding: 5%;
   display: flex;
   flex-direction: column;
@@ -12,6 +13,7 @@ export const Box = styled.div`
   -webkit-backdrop-filter: blur( 4px );
   border-radius: 10px;
   @media(max-width: 1024px) {
+    width: 90%;
     padding: 10%;
   }
 `
